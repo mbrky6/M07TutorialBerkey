e1011b0f800b1e3e8157c964cc8a2f162849a45f
mbrky6 <mberkey6@ivytech.edu>
2023-03-01 20:23:10 -0500

    Added a hook function to hash passwords before storing them.

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const {isEmail} = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,14 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please enter a password"],
         minlength: [6, "Password must be at least 6 characters long"]
     }
+});
+
+// Hook: Function before saving to database (hook after saving is called x.post)
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
 });
 
 const User = mongoose.model("user", userSchema);
