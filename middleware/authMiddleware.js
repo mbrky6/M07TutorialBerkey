08fea48d859cf2f4b3142f2ae03e12f6d3ffe101
mbrky6 <mberkey6@ivytech.edu>
2023-03-03 15:35:41 -0500

    Made the header dynamic using the login credential verification function.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -22,4 +23,28 @@ const requireAuth = (req, res, next) => {
 
 } // const requireAuth
 
-module.exports = {requireAuth};
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "This totally won't get posted to GitHub", async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } // if (token is invalid)
+            else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            } // else
+        }); // Again, don't post secrets to GitHub
+    } // if (token exists)
+    else {
+        res.locals.user = null;
+        next();
+    } // else
+} // const CheckUser
+
+module.exports = {requireAuth, checkUser};
